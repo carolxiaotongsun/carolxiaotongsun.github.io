96078dbfe82ac7d8a30294ca677007b18bea3303
carolxiaotongsun <sun_xiao_tong@ruc.edu.cn>
2020-01-05 02:06:45 +0800

    Update mine.js

--- mine.js
+++ mine.js
@@ -3,16 +3,20 @@ $(document).ready(function (){
         var X = $('#INTRODUCTION_PART').offset().top;
         window.scrollTo(0, X - 70); 
     }
-    document.getElementById("BUT_EXPERIENCE_PART").onclick = function () {
-        var X = $('#EXPERIENCE_PART').offset().top;
+    document.getElementById("BUT_PROFEXPERIENCE_PART").onclick = function () {
+        var X = $('#PROFEXPERIENCE_PART').offset().top;
         window.scrollTo(0, X - 70); 
     }
     document.getElementById("BUT_RESEARCH_INTERE_PART").onclick = function () {
         var X = $('#RESEARCH_INTERE_PART').offset().top;
         window.scrollTo(0, X - 70); 
     }
-    document.getElementById("BUT_RESEARCH_PUB_PART").onclick = function () {
-        var X = $('#RESEARCH_PUB_PART').offset().top;
+    document.getElementById("BUT_SKILL_PART").onclick = function () {
+        var X = $('#SKILL_PART').offset().top;
+        window.scrollTo(0, X - 70); 
+    }
+    document.getElementById("BUT_EDUCATION_PART").onclick = function () {
+        var X = $('#EDUCATION_PART').offset().top;
         window.scrollTo(0, X - 70); 
     }
 })
